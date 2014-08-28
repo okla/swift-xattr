c33be14596e8bf678b146fcc628a69918889ec80
okla <admin@Mac-Admin.local>
2014-08-28 13:54:35 +0400

    Minor changes

--- ExtendedAttributes.swift
+++ ExtendedAttributes.swift
@@ -6,12 +6,11 @@ import Foundation
   :returns: Error description
 */
 func errnoDescription() -> String {
-  
   return NSString(UTF8String: strerror(errno))
 }
 
 /**
-  Set data for extended attribute at path
+  Set extended attribute at path
 
   :param: name Name of extended attribute
   :param: data Data for extended attribute
@@ -20,7 +19,6 @@ func errnoDescription() -> String {
   :returns: In case of success return nil, in case of fail return error description
 */
 func setAttributeWithName(name: String, #data: NSData, atPath path: String) -> String? {
-  
   return setxattr(path, name, data.bytes, UInt(data.length), 0, 0) == -1 ? errnoDescription() : nil
 }
 
@@ -33,57 +31,41 @@ func setAttributeWithName(name: String, #data: NSData, atPath path: String) -> S
   :returns: Tuple with error description and attribute data. In case of success first parameter is nil, in case of fail second.
 */
 func dataForAttributeNamed(name: String, atPath path: String) -> (error: String?, data: NSData?) {
-  
   let bufLength = getxattr(path, name, nil, 0, 0, 0)
-    
   if bufLength == -1 {
-    
     return (errnoDescription(), nil)
   }
   else {
-    
     var buf = malloc(UInt(bufLength))
-      
     if getxattr(path, name, buf, UInt(bufLength), 0, 0) == -1 {
-        
       return (errnoDescription(), nil)
     }
     else {
-        
       return (nil, NSData(bytes: buf, length: bufLength))
     }
   }
 }
 
 /**
-  Get names of extended attributes associated with path
+  Get names of extended attributes at path
 
   :param: path Path to file, directory, symlink etc
 
   :returns: Tuple with error description and array of extended attributes names. In case of success first parameter is nil, in case of fail second.
 */
 func attributesNamesAtPath(path: String) -> (error: String?, names: [String]?) {
-    
   let bufLength = listxattr(path, nil, 0, 0)
-    
   if bufLength == -1 {
-      
     return (errnoDescription(), nil)
   }
   else {
-      
     var buf = UnsafeMutablePointer<Int8>(malloc(UInt(bufLength)))
-      
     if listxattr(path, buf, UInt(bufLength), 0) == -1 {
-        
       return (errnoDescription(), nil)
     }
     else {
-        
       var names = NSString(bytes: buf, length: bufLength, encoding: NSUTF8StringEncoding).componentsSeparatedByString("\0")
-        
       names.removeLast()
-        
       return (nil, (names as [String]))
     }
   }
@@ -98,6 +80,5 @@ func attributesNamesAtPath(path: String) -> (error: String?, names: [String]?) {
   :returns: In case of success return nil, in case of fail return error description
 */
 func removeAttributeNamed(name: String, atPath path: String) -> String? {
-    
   return removexattr(path, name, 0) == -1 ? errnoDescription() : nil
 }
